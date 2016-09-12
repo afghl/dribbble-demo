195b9a64e627757d85c39edca95a4385fb0c2ea3
afghl <airport.0930@gmail.com>
2016-09-12 17:10:15 +0800

    map description to shot

--- containers/shot_detail/Desc.js
+++ containers/shot_detail/Desc.js
@@ -2,11 +2,10 @@ import React, { Component } from 'react'
 
 export default class ShotDetailDesc extends Component {
   render() {
+    const { description } = this.props.shot
+
     return (
-      <div className="shot-desc">
-        <p>Hello dribbblers,</p>
-        <p>Become a seller at MarketMe and enjoy many incredible features created to give you the best experience. One of them is the Dashboard that you can use to manage your products, your sales and track the feedbacks.</p>
-        <p><a href="http://www.market-me.fr/" rel="nofollow noreferrer">MarketMe V3</a></p>
+      <div className="shot-desc" dangerouslySetInnerHTML={{__html: description}}>
       </div>
     )
   }
--- containers/shot_detail/TagSession.js
+++ containers/shot_detail/TagSession.js
@@ -6,14 +6,14 @@ export default class ShotDetailTagSession extends Component {
       <div className="tags-section">
         <h3 className="meta-head">Tags</h3>
         <ol id="tags" className="popular-tags">
-          <li id="tag-li-501" className="tag">badge</li>
-          <li id="tag-li-7093" className="tag">boy</li>
-          <li id="tag-li-40797" className="tag">cub</li>
-          <li id="tag-li-1296" className="tag">happy</li>
-          <li id="tag-li-225" className="tag">illustration</li>
-          <li id="tag-li-6539" className="tag">scout</li>
-          <li id="tag-li-713" className="tag">smile</li>
-          <li id="tag-li-3907" className="tag">sun</li>
+          <li className="tag">badge</li>
+          <li className="tag">boy</li>
+          <li className="tag">cub</li>
+          <li className="tag">happy</li>
+          <li className="tag">illustration</li>
+          <li className="tag">scout</li>
+          <li className="tag">smile</li>
+          <li className="tag">sun</li>
         </ol>
       </div>
     )
