086c5a77f643fd9d587f5593a8484da598bf470e
afghl <airport.0930@gmail.com>
2017-06-19 23:36:32 +0800

    toggle zooming in detail page

--- containers/shot_detail/ShotImage.js
+++ containers/shot_detail/ShotImage.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react'
 
 export default class ShotDetailShotImage extends Component {
+  constructor(props) {
+    super(props)
+    this.toggleZooming = this.toggleZooming.bind(this)
+    this.state = this.getState()
+  }
+
+  getState() {
+    return { isZooming: false }
+  }
+
+  toggleZooming() {
+    this.setState({ isZooming: !this.state.isZooming })
+  }
+
   render() {
     const { images, title } = this.props.shot
     const { hidpi, normal } = images
+    const { state: { isZooming }, toggleZooming } = this
     return (
-      <div className="the-shot">
+      <div
+        className={isZooming ? 'the-shot zooming' : 'the-shot'}
+        onClick={toggleZooming}
+      >
         <div className="single group">
           <div className="single-grid">
             <div className="single-img">
